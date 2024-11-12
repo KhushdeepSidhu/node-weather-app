09f471332252743cf24666d591501a20fe984fb7
sidhu <khushdeep>
2024-11-12 16:07:33 -0500

    updates for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,27 @@
 // HTTP request
-const getData = async ( address ) => {
-    const response = await fetch ( `/weather?address=${address}` )
-    return await response.json ()
-}
+const getData = async (address) => {
+  const response = await fetch(`/weather?address=${address}`);
+  return await response.json();
+};
 
 // Setup Elements
-const weatherFormEl = document.querySelector( 'form' )
-const search = document.querySelector ( 'input' )
-const forecastMessageEl = document.querySelector ( '#forecast' )
-const locationMessageEl = document.querySelector ( '#location' )
+const weatherFormEl = document.querySelector('form');
+const search = document.querySelector('input');
+const forecastMessageEl = document.querySelector('#forecast');
+const locationMessageEl = document.querySelector('#location');
 
-// Add event listener for form 
-weatherFormEl.addEventListener ( 'submit', async ( event ) => {
-    
-    event.preventDefault()
+// Add event listener for form
+weatherFormEl.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    forecastMessageEl.textContent = 'Loading...'
-    locationMessageEl.textContent = ''
+  forecastMessageEl.textContent = 'Loading...';
+  locationMessageEl.textContent = '';
 
-    const data = await getData ( search.value )
-    if ( data.error ) {
-        return forecastMessageEl.textContent = data.error
-    }
+  const data = await getData(search.value);
+  if (data.error) {
+    return (forecastMessageEl.textContent = data.error);
+  }
 
-    forecastMessageEl.textContent = data.forecastData
-    locationMessageEl.textContent = data.location
-
-} ) 
+  forecastMessageEl.textContent = data.forecastData;
+  locationMessageEl.textContent = data.location;
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,23 @@
-const request = require ( 'request' )
+const request = require('request');
 
-const forecast = ( latitude, longitude, callback ) => {
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=${latitude},${longitude}`;
 
-    const url = `https://api.darksky.net/forecast/b461f65102fe8960c64ef744d2bc1592/${latitude},${longitude}?units=si`
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (response.body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          '. It is currently ' +
+          response.body.current.temperature +
+          ' degress out.'
+      );
+    }
+  });
+};
 
-    request ( { url, json: true }, ( error, { body } ) => {
-        if ( error ) {
-            callback ( 'Unable to connect to the weather API.' )
-        } else if ( body.error ) {
-            callback ( 'Unable to find the location.' )
-        } else {
-            const forecast = `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability * 100 }% chance of rain.The temperature high is ${body.daily.data[0].temperatureHigh} and temperature low is ${body.daily.data[0].temperatureLow}.`
-            callback ( undefined, forecast )
-        }
-    } )
-}
-
-module.exports = forecast
+module.exports = forecast;
